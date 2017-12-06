77c61e02b67e2fcdda3988cdaba3b251558071f9
Miłosz Piechocki <miloszpp@gmail.com>
2017-12-06 22:27:44 +0100

    Uzycie AsyncPipe w band-list.component

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -1,35 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Band } from "../model";
 import { BandService } from "../band.service";
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-band-list',
   templateUrl: './band-list.component.html',
-  styles: []
 })
 export class BandListComponent implements OnInit {
-
   band: Band = null;
-  bands: Band[];
+  bands$: Observable<Band[]>;
+  click$ = new Subject<any>();
 
-  constructor(private bandsService: BandService) {
-  }
+  constructor(private bandsService: BandService) {}
   
   ngOnInit(): void {
-    this.bandsService.getBands().subscribe(
-      bands => { this.bands = bands; },
-      error => { alert("Błąd podczas pobierania listy zespołów"); }
-    );
+    this.bands$ = Observable.merge(
+      this.bandsService.getBands(), // inicjalne załadowanie danych
+      this.click$.debounceTime(500).mergeMap(() => this.bandsService.getBands()) // kolejne strzały juz tylko po kliknieciu
+    ).do(() => {
+      this.band = null; // zawsze jak dostaniemy nową listę zespołów to resetujemy obecnie wybrany
+    });
   }
-  
-  shiftBand(direction) {
-    let currentIdx = this.bands.indexOf(this.band);
-    if (direction === "previous" && currentIdx > 0) {
-      this.band = this.bands[currentIdx - 1];
-    }
-    if (direction === "next" && currentIdx < this.bands.length - 1) {
-      this.band = this.bands[currentIdx + 1];
-    }
-  }
-
 }
